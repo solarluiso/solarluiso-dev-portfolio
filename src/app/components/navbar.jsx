0a70ff9bc6669786c5ae080fe9d068adf9f69d10
Luis <solarluiso@gmail.com>
2024-05-03 18:51:36 -0400

    navbar logo

---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* LOGO */}
+      <div className="">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mr-1">Luiso</span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+            .dev
+          </span>
+        </Link>
+      </div>
+
+      {/* RESPONSIVE MENU */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
